19ffe5b09c2c1bd4e683a7a38cb4d496c68d9d4d
Muna <manritter@uos.de>
2015-12-14 23:37:33 +0100

    new virtual environment due to new location on computer

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Applications/MAMP/htdocs/venv"
+set -gx VIRTUAL_ENV "/Users/Muna/EMA/htdocs/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
